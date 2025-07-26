8dc55ef5fe0106f9b7327b14b1829edb4b9c6ae7
Devang Jain <devangjain1221@gmail.com>
2025-07-26 22:16:08 +0530

    Edited the Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <Link to="/">
-                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+                <p className="text-2xl font-bold text-gradient">RESUMIND - The AI mind for your resume</p>
             </Link>
             <Link to="/upload" className="primary-button w-fit">
                 Upload Resume
